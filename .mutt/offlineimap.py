a26e27207f7c8ff8cc4e1df3790c1d06d3c0fd95
Beau Lyddon <beau.lyddon@webfilings.com>
2012-12-10 19:45:29 -0600

    Add mutt settings

---
+++ .mutt/offlineimap.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pyton
+
+import re, subprocess
+
+def get_keychain_pass(account=None, server=None):
+    params = {
+        'security': '/usr/bin/security',
+        'command': 'find-internet-password',
+        'account': account,
+        'server': server,
+        'keychain': '/Users/beaulyddon/Library/Keychains/login.keychain',
+    }
+    command = "sudo -u beaulyddon %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    outtext = [l for l in output.splitlines()
+                if l.startswith('password: ')][0]
+
+    return re.match(r'password: "(.*)"', outtext).group(1)
